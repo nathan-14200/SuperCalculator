85abe7aef0b31b967694678f303a013f88edd0c3
nathan-14200 <nathan_3145@hotmail.com>
2017-12-08 15:47:44 +0100

    Get Add class

---
+++ FunctionLibrary/FunctionLibrary/Add.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionLibrary
+{
+    class Add : Function<string>
+    {
+        string Function<string>.Name
+        {
+            get
+            {
+                return "Add";
+            }
+        }
+
+        string Function<string>.HelpMessage
+        {
+            get
+            {
+                return "Does the sum of 2 numbers";
+            }
+        }
+
+        string[] Function<string>.ParametersName
+        {
+            get
+            {
+                string[] param = new string[2];
+                param.SetValue("float a", 0);
+                param.SetValue("float b", 1);
+
+                return param;
+            }
+        }
+
+        public string Evaluate(string[] args)
+        {
+            try
+            {
+                float a = float.Parse(args[0]);
+                float b = float.Parse(args[1]);
+
+                string result = (a + b).ToString();
+                return result;
+            }
+            catch
+            {
+                return "Could not compute Add function";
+            }
+        }
+    }
+}
