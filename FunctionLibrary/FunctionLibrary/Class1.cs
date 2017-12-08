bd60f05b68784e636a8888949b000122c3f7da7c
nathan-14200 <nathan_3145@hotmail.com>
2017-12-08 15:48:35 +0100

    Getting the interface to implement

--- FunctionLibrary/FunctionLibrary/Class1.cs
+++ FunctionLibrary/FunctionLibrary/Class1.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace FunctionLibrary
 {
-    public class Class1
+    public interface Function<T>
     {
+
+        string Name { get; }
+        //Get the name of the function
+
+        string HelpMessage { get; }
+        //Tells what the function does and give param format
+
+        string[] ParametersName { get; }
+        //Get the list of param
+
+        T Evaluate(string[] args);
+        //Evaluate function with given param or throw exception
     }
+
+    public class EvaluationException : Exception
+    {
+        public EvaluationException(string msg) : base(msg)
+        {
+
+        }
+    }
+
 }
