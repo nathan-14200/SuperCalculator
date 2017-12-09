b9c80473ec359e5d5e26358fd799e2aa5c0f66e8
JeanVanneste <jean.vanneste@student.vinci.be>
2017-12-09 18:33:15 +0100

    Function Factorial
    Attention implémentation à finir

---
+++ FunctionLibrary/FunctionLibrary/Factorial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FunctionLibrary
+{
+    /*
+    class Factorial : Function<string>
+    {
+        string Function<string>.Name
+        {
+            get { return "Factorial"; }
+        }
+
+        string Function<string>.HelpMessage
+        {
+            get
+            {
+                return "This function return the factorial value of an integer";
+            }
+        }
+
+        string[] Function<string>.ParametersName
+        {
+            get
+            {
+                return new[] { "Integer" };
+            }
+        }
+
+        string Evaluate(string[] args)
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+    [TestFixture()]
+    public class TestFactorial
+    {
+        [Test()]
+        public void TestName()
+        {
+            Factorial factorial = new Factorial();
+            Assert.That(Factorial., Is.EqualTo("Factorial"));
+        }
+    }
+    */
+}
