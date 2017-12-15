42ebdea3e4bff6471d176b3d06bec249e29e737a
JeanVanneste <jean.vanneste@student.vinci.be>
2017-12-15 01:33:21 +0100

    Récupération DLL superComputer

---
+++ FunctionLibrary/FunctionLibrary/Function.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperComputer;
+
+namespace FunctionLibrary
+{
+    public interface Function<T> : IFunction
+    {
+        T Evaluate(string[] args);
+    }
+
+}
---
+++ FunctionLibrary/FunctionLibrary/IFunction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionLibrary
+{
+    // SuperComputer.IFunction
+    public interface IFunction
+    {
+        string Name
+        {
+            get;
+        }
+
+        string HelpMessage
+        {
+            get;
+        }
+
+        string[] ParametersName
+        {
+            get;
+        }
+    }
+
+}
